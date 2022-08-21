3dd8257fecbbcaad2b6c8458f9dd517b9144fb45
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-08-21 07:40:56 +0000

    [update]

--- main.rb
+++ main.rb
@@ -1,9 +1,5 @@
 class Brave
   # attr_readerの記述でゲッターを省略することができる
-  # attr_reader :name
-  # attr_reader :hp
-  # attr_reader :offense
-  # attr_reader :defense
   attr_reader :name, :offense, :defense
 
   attr_accessor :hp
@@ -25,9 +21,9 @@ class Brave
     # decision_attack_typeメソッドの呼び出し
     attack_type = decision_attack_type
     # calculate_damageメソッドの呼び出し キーワード引数を設定
-    damege = calculate_damage(target: monster, attack_type: attack_type)
+    damage = calculate_damage(target: monster, attack_type: attack_type)
     # ダメージをHPに反映させる キーワード引数を設定
-    cause_damage(target: monster, damege: damege)
+    cause_damage(target: monster, damage: damage)
 
     puts "#{monster.name}の残りHPは#{monster.hp}だ"
   end
@@ -63,21 +59,19 @@ class Brave
    # HPにダメージを反映させる
   def cause_damage(**params)
      # 変数に格納することによって将来ハッシュのキーに変更があった場合でも変更箇所が少なくて済む
-    damege = params[:damege]
+    damage = params[:damage]
     target = params[:target]
 
-    target.hp -= damege
-    puts "#{target.name}は#{damege}のダメージを受けた"
+    target.hp -= damage
+    # もしターゲットのHPがマイナスになるなら0を代入
+    target.hp = 0 if target.hp < 0
+
+    puts "#{target.name}は#{damage}のダメージを受けた"
   end
 
   def calculate_special_attack
     @offense * SPECIAL_ATTACK_CONSTANT
   end
-
-  # # セッター再定義
-  # def hp=(hp)
-  #   @hp = hp
-  # end
 
 end
 
@@ -114,17 +108,37 @@ class Monster
 
     puts "#{@name}の攻撃"
 
-    # 勇者に与えるダメージの計算
-    damege = @offense - brave.defense
-    # 勇者クラスのHPにダメージを反映
-    brave.hp = damege
+     # ダメージ計算処理の呼び出し
+    damage = calculate_damage(brave)
 
-    puts "#{brave.name}は#{damege}のダメージを受けた"
+    # ダメージ反映処理の呼び出し
+    cause_damage(target: brave, damage: damage)
+
+    puts "#{brave.name}は#{damage}のダメージを受けた"
     puts "#{brave.name}の残りのHPは#{brave.hp}だ"
   end
 
   # クラス外から呼び出せないようにする
   private
+  # ダメージ計算処理
+  def calculate_damage(target)
+    @offense - target.defense
+  end
+
+  # ダメージ反映処理
+  def cause_damage(**params)
+    # 引数で受け取った値を変数に格納
+    damage = params[:damage]
+    target = params[:target]
+
+    target.hp -= damage
+
+    # もしターゲットのHPがマイナスになるなら0を代入
+    target.hp = 0 if target.hp < 0
+
+    puts "#{target.name}は#{damage}のダメージを受けた"
+  end
+
   # 変身メソッド
   def transform
     # 変身後の名前
@@ -148,21 +162,20 @@ brave = Brave.new(name: "テリー", hp: 500, offense: 150, defense: 100)
 
 monster = Monster.new(name: "スライム", hp: 200, offense: 200, defense: 100)
 
-brave.attack(monster)
-monster.attack(brave)
-# puts "NAME：#{brave.name}"
-# puts "HP：#{brave.hp}"
-# puts "OFFENSE：#{brave.offense}"
-# puts "DEFENSE：#{brave.defense}"
+loop do
+  brave.attack(monster)
+  if monster.hp <= 0
+    exp = (monster.offense + monster.defense) * 2
+    gold = (monster.offense + monster.defense) * 3
+    puts "#{brave.name}はたたかいに勝った"
+    puts "#{exp}の経験値と#{gold}ゴールドを獲得した"
+    break
+  end
 
-# 上記と同じ
-# puts <<~TEXT
-# NAME：#{brave.name}
-# HP：#{brave.hp}
-# OFFENSE：#{brave.offense}
-# DEFENSE：#{brave.defense}
-# TEXT
-
-# brave.hp -= 30
-
-# puts "#{brave.name}はダメージを受けた!残りHPは#{brave.hp}だ"
+  monster.attack(brave)
+  if brave.hp <= 0
+    puts "#{brave.name}はたたかいに負けた"
+    puts "目の前が真っ暗になった"
+    break
+  end
+end
